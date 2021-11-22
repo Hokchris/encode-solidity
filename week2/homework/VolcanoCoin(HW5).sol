cb61867d3e4eb9eb27fcd3dd5d31af91aedf9979
Christian <christian.honkanen@gmail.com>
2021-11-22 15:59:07 +0000

    hw 5 final

--- week2/homework/VolcanoCoin(HW5).sol
+++ week2/homework/VolcanoCoin(HW5).sol
@@ -6,30 +6,45 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract VolcanoCoin is ERC20, Ownable {
     
-    uint tokenSupply = 10000;
+    string private TOK_NAME = "VolcanoCoin";
+    string private TOK_SYMBOL = "VOLC";
+    uint private tokenSupply = 10000;
     
-    mapping(address => Payment[]) payments;
+    event SupplyChange(uint _newSupply);
+    
+    mapping(address => Payment[]) public payments;
     
     struct Payment {
         uint amount;
         address recipientAddress;
     }
     
-    constructor(string memory _tokenName, string memory _symbol) ERC20(_tokenName, _symbol) {
+    constructor() ERC20(TOK_NAME, TOK_SYMBOL) {
         _mint(msg.sender, tokenSupply);
     }
     
-    function giveToOwner(uint _amount) public onlyOwner {
-        _mint(owner(), _amount);    
+    function addToSupply(uint _amount) public onlyOwner {
+        _mint(msg.sender, _amount);
+        tokenSupply += _amount;
+        emit SupplyChange(tokenSupply);
     }
     
     function getSupply() public view returns(uint) {
         return tokenSupply;
     }
     
-    // function getPayments(address _address) public view returns (Payment[] memory) {
-    //     return payments[_address];
-    // }
+    function makeTransfer(address _sender, address _receiver, uint _amount) public {
+        require(msg.sender == _sender);
+        require(balanceOf(msg.sender) >= _amount, "Not enough funds.");
+        
+        transfer(_receiver, _amount);
+        Payment memory _rec = Payment({amount: _amount, recipientAddress: _receiver});
+        payments[msg.sender].push(_rec); 
+
+    }
+    
+    function getPayments(address _address) public view returns(Payment[] memory) {
+        return payments[_address];
+    }
 
 }
-
