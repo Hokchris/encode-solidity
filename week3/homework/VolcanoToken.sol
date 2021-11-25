486090455d7f53809bc7a3fbce51dd517974204c
Christian <christian.honkanen@gmail.com>
2021-11-25 22:23:47 +0000

    hw6 last parts

--- week3/homework/VolcanoToken.sol
+++ week3/homework/VolcanoToken.sol
@@ -18,7 +18,6 @@ contract VolcanoToken is ERC721("VolcanoToken", "VOLT"), Ownable {
     mapping(address => Metadata[]) public tokenOwnership;
     
     function mintToken(string memory _tokenURI) public {
-        
         _safeMint(msg.sender, tokenId);
         
         Metadata memory tokenMetadata = Metadata(
@@ -31,7 +30,6 @@ contract VolcanoToken is ERC721("VolcanoToken", "VOLT"), Ownable {
         
         tokenOwnership[msg.sender].push(tokenMetadata);
         tokenId += 1;
-        
     }
     
     function burnToken(uint _tokenId) public {
@@ -41,23 +39,36 @@ contract VolcanoToken is ERC721("VolcanoToken", "VOLT"), Ownable {
     }
     
     function removeToken(uint _tokenId) internal {
-        Metadata[] memory userTokens = tokenOwnership[msg.sender];
+        uint numTokens = tokenOwnership[msg.sender].length;
         
-        for (uint i=0; i < userTokens.length; i++) {
-            if (_tokenId == userTokens[i].tokenId) {
-                delete tokenOwnership[msg.sender][i];
+        for (uint i=0; i < numTokens; i++) {
+            if (_tokenId == tokenOwnership[msg.sender][i].tokenId) {
+                // delete tokenOwnership[msg.sender][i];  // delete seems to set all values to default, ie uints -> 0 etc.
+                
+                // if token to remove is not last in struct array, set it to have value of last item in array
+                if (i != numTokens - 1) {
+                    tokenOwnership[msg.sender][i] = tokenOwnership[msg.sender][numTokens - 1];
+                }
+                
+                tokenOwnership[msg.sender].pop();
+                break;
             }
         }
-    
     }
     
     function tokenURI(uint _tokenId) public view override returns (string memory) {
         require(_exists(_tokenId), "Invalid token ID");
-        return "a";
+        Metadata[] memory userTokens = tokenOwnership[ownerOf(_tokenId)];
+        
+        for (uint i=0; i < userTokens.length; i++) {
+            if (_tokenId == userTokens[i].tokenId) {
+                return userTokens[i].tokenURI;
+            }
+        }
     }
     
-    function test(address _addr) public view returns (Metadata[] memory) {
-        return tokenOwnership[_addr];
+    function userTokenOwnerships(address _address) public view returns (Metadata[] memory) {
+        return tokenOwnership[_address];
     }
     
 }
