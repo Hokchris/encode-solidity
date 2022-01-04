b5a48b62eb0ef710fff7bc24581925b210ffa02b
Christian <christian.honkanen@gmail.com>
2022-01-04 01:55:53 +0000

    hw7

--- week3/homework/hw7/src/app.js
+++ week3/homework/hw7/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
-// const ipfsAPI = require("../public/javascript/ipfsAPI");
-
+const ipfsApi = require("../public/javascript/ipfsApi.js");
 const app = express();
-
 const publicPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicPath, { extensions: ["html"] }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 
 app.get("", (req, res) => {
   res.sendFile(publicPath + "/frontPage.html");
@@ -14,14 +16,54 @@ app.listen(3000, () => {
   console.log(`Running express app at http://127.0.0.1:3000`);
 });
 
-app.post("/storeData", (req, res) => {
-  ipfsAPI.ipfsStore(req.body.data).then((response) => {
-    res.send(response);
-  });
+app.post("/addData", (req, res) => {
+  ipfsApi
+    .ipfsAdd(req.body.data)
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((e) => {
+      res.status(500, {
+        error: e,
+      });
+    });
 });
 
 app.post("/getData", (req, res) => {
-  ipfsAPI.ipfsGet(req.body.data).then((response) => {
-    res.send(response);
-  });
+  ipfsApi
+    .ipfsGet(req.body.data)
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((e) => {
+      res.status(500, {
+        error: e,
+      });
+    });
+});
+
+app.post("/getImage", (req, res) => {
+  ipfsApi
+    .ipfsGetImage(req.body.data)
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((e) => {
+      res.status(500, {
+        error: e,
+      });
+    });
+});
+
+app.post("/addFile", (req, res) => {
+  ipfsApi
+    .addFile(req.body.data)
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((e) => {
+      res.status(500, {
+        error: e,
+      });
+    });
 });
