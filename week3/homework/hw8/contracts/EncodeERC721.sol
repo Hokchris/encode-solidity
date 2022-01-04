35217b2e1fbca0050034257140e3e4117ba18b33
Christian <christian.honkanen@gmail.com>
2022-01-04 02:19:08 +0000

    hw8 contract

--- week3/homework/hw8/contracts/EncodeERC721.sol
+++ week3/homework/hw8/contracts/EncodeERC721.sol
@@ -10,18 +10,28 @@ contract EncodeToken is ERC721URIStorage {
 
   constructor() ERC721("EncodeToken", "ENC") {}
 
-  function mintToken(address tokenOwner, string memory tokenURI) 
-    public
-    returns (uint256)
-  {
+  mapping(address => uint256[]) internal nfts;
+
+  function mintToken(address _tokenOwner, string memory _tokenURI) public returns (uint256) {
     _tokenIds.increment();
 
     uint256 newItemId = _tokenIds.current();
-    _mint(tokenOwner, newItemId);
-    _setTokenURI(newItemId, tokenURI);
-
+    _mint(_tokenOwner, newItemId);
+    _setTokenURI(newItemId, _tokenURI);
+    nfts[_tokenOwner].push(newItemId);
     return newItemId;
   }
 
+  function getNfts(address _tokenOwner) public view returns (uint256[] memory) {
+    return nfts[_tokenOwner];
+  }
+
+  function transferNft(address _oldOwner, address _newOwner, uint256 _index) public {
+    safeTransferFrom(_oldOwner, _newOwner, _index);
+
+    uint _newIndex = _index - 1;
+    nfts[_newOwner].push(nfts[_oldOwner][_newIndex]);
+    delete nfts[_oldOwner][_newIndex];
+  }
 
 }
