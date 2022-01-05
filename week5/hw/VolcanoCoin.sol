7f93815ee6a7e1244c9f16793a2508e7640b1f58
Christian <christian.honkanen@gmail.com>
2022-01-05 11:22:24 +0000

    readable transfer function with events

--- week5/hw/VolcanoCoin.sol
+++ week5/hw/VolcanoCoin.sol
@@ -57,12 +57,28 @@ contract VolcanoCoin is ERC20("VolcanoCoin", "VLC"), Ownable {
 
   }
   
-  function getPayments(address _address) public view returns(Payment[] memory) {
-    return payments[_address];
-  }
+//   function getPayments(address _address) public view returns(Payment[] memory) {
+//     return payments[_address];
+//   }
 
   function viewPaymentDetails() public view returns (Payment[] memory) {
     return payments[msg.sender];
   }
+
+  event SinglePayment(Payment payment);
+  function viewPaymentDetailsReadable() public {
+    Payment[] memory allPayments = payments[msg.sender];
+    
+    require(allPayments.length > 0, "No payments to show.");
+    for (uint i=0; i < allPayments.length; i++) {
+      Payment memory currPayment = allPayments[i];
+      emit SinglePayment(currPayment);
+    }
+  }
+
+  // 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+
+  // 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+  // 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
 
 }
