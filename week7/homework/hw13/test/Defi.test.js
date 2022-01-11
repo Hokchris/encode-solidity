f981ca247c2655bc1d89a2eebe37c336d7430898
Christian <christian.honkanen@gmail.com>
2022-01-11 15:24:30 +0000

    hw13 complete

--- week7/homework/hw13/test/Defi.test.js
+++ week7/homework/hw13/test/Defi.test.js
@@ -14,11 +14,10 @@ describe("DeFi", () => {
   let DAI_TokenContract;
   let USDC_TokenContract;
   let DeFi_Instance;
-  const TRANSFER_QUANTITY = 99999999900000;
+  const INITIAL_AMOUNT = 99999999900000;
   before(async function () {
     [owner, addr1, addr2, addr3, addr4, addr5] = await ethers.getSigners();
 
-    // didn't work when running on terminal on own PC but worked with gitpod (??)
     const whale = await ethers.getSigner(unlockedAddress);
 
     console.log("owner account is ", owner.address);
@@ -31,13 +30,64 @@ describe("DeFi", () => {
     const DeFi = await ethers.getContractFactory("DeFi");
     await DAI_TokenContract.connect(whale).transfer(
       owner.address,
-      BigInt(TRANSFER_QUANTITY)
+      BigInt(INITIAL_AMOUNT)
     );
 
     DeFi_Instance = await DeFi.deploy();
   });
 
-  it("should check transfer succeeded", async () => {});
-  it("should sendDAI to contract", async () => {});
-  it("should make a swap", async () => {});
+  it("should check transfer succeeded", async () => {
+    let daiBalance = await DAI_TokenContract.balanceOf(owner.address);
+    // let daiBalanceCoinbase = await DAI_TokenContract.balanceOf(unlockedAddress);
+
+    // console.log("DeFi contract DAI balance: ", ethers.utils.formatUnits(daiBalance, 18));
+    // console.log("Coinbase contract DAI balance: ", ethers.utils.formatUnits(daiBalanceCoinbase, 18));
+
+    expect(daiBalance > 0);
+  });
+  it("should sendDAI to contract", async () => {
+    // let daiBalanceBefore = await DAI_TokenContract.balanceOf(owner.address);
+
+    await DAI_TokenContract.transfer(
+      DeFi_Instance.address,
+      BigInt(INITIAL_AMOUNT),
+      {
+        from: owner.address,
+        gasPrice: 0,
+      }
+    );
+
+    /**
+    let daiBalanceAfter = await DAI_TokenContract.balanceOf(owner.address);
+
+    console.log("Before: ", ethers.utils.formatUnits(daiBalanceBefore, 18));
+    console.log("After: ", ethers.utils.formatUnits(daiBalanceAfter, 18));
+    
+    let diff =  daiBalanceBefore - daiBalanceAfter;
+    
+    console.log(INITIAL_AMOUNT, " -> ", diff);
+
+    expect(diff == INITIAL_AMOUNT);
+    */
+  });
+  it("should make a swap", async () => {
+    // let usdcBalance = await USDC_TokenContract.balanceOf(owner.address);
+    // let daiBalance = await DAI_TokenContract.balanceOf(DeFi_Instance.address);
+
+    // console.log("USDC: ", usdcBalance);
+    // console.log("DAI: ", daiBalance);
+
+    await DeFi_Instance.swapDAItoUSDC(9999999990000, {
+      from: owner.address,
+      gasPrice: 0,
+    });
+
+    let usdcBalance2 = await USDC_TokenContract.balanceOf(owner.address);
+    // let daiBalance2 = await DAI_TokenContract.balanceOf(DeFi_Instance.address);
+
+    // console.log("USDC: ", usdcBalance2);
+    // console.log("DAI: ", daiBalance2);
+
+    expect(usdcBalance2.toNumber() > 0);
+  });
 });
